d627962f5404aa8267be5bb5c36fe0e30c02fccd
ToeWai27 <100681046+ToeWai27@users.noreply.github.com>
2022-12-29 10:16:49 +0630

    commit for version control and add small changes

---
+++ constants/index.js
@@ -0,0 +1,12 @@
+module.exports.tasks = [
+  { id: 1, name: "Work", completed: false, userId: "3" },
+  { id: 2, name: "Learning", completed: true, userId: "4" },
+  { id: 3, name: "Practicing", completed: false, userId: "2" },
+  { id: 4, name: "GYM", completed: false, userId: "1" },
+];
+module.exports.users = [
+  { id: 1, name: "toe", email: "toe@gmail.com" },
+  { id: 2, name: "wai", email: "wai@gmail.com" },
+  { id: 3, name: "phyo", email: "phyo@gmail.com" },
+  { id: 4, name: "elsie", email: "elsie@gmail.com" },
+];
---
+++ resolvers/index.js
@@ -0,0 +1,3 @@
+const UserResolver = require("./user");
+const TaskResolver = require("./task");
+module.exports = [UserResolver, TaskResolver];
